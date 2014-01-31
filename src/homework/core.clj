df52354c74a26dbcf9c59d5f72e268cc69b4c88d
kevinbuch <kevaustinbuch@gmail.com>
2014-01-31 14:55:15 -0600

    nice

--- src/homework/core.clj
+++ src/homework/core.clj
@@ -1,29 +1,34 @@
 (ns homework.core)
 
 (defn find-node [predicate tree]
+  (if (predicate tree)
+    tree
+    (if tree
+      (->> (map #(find-node predicate %) (:children tree))
+           (drop-while not)
+           first))))
+
+(defn find-all [predicate tree]
+  (let [found (if (predicate tree) #{tree} #{})]
+    (->> (map #(find-all predicate %) (:children tree))
+         (reduce clojure.set/union #{})
+         (clojure.set/union found))))
+
+(defn find-node-custom [predicate get-children tree]
+  (let [children (get-children tree)]
     (if (predicate tree)
       tree
       (if tree
-        (first (map #(find-node predicate %) (:children tree))))))
+        (->> (map #(find-node-custom predicate get-children %) children)
+             (drop-while not)
+             first)))))
 
-(defn find-all [predicate tree]
-  #{})
+(defn find-node-with-path [path-so-far path-predicate get-children tree]
+  (let [so-far (conj path-so-far tree)]
+    (if (path-predicate so-far)
+      tree
+      (if tree
+        (->> (map #(find-node-with-path so-far path-predicate get-children %) (get-children tree))
+             (drop-while not)
+             first)))))
 
-(defn find-node-custom [predicate get-children tree]
-  ;you will probably say something like:
-  ;(let [children (get-children tree)])
-  [])
-
-
-(defn find-node-with-path [path-so-far path-predicate get-children tree])
-;CONTRACT
-;if it's in the subree
-  ;return it
-;if it's not in the subtee
-  ;return nil
-
-;IDEA:
-; if predicate tree
-;  tree
-;  else
-; get the children of the tree and iterate through them, checking each one
